ff95f0f787bc254582c2fb32325ee340d62f8507
tantantanxinyi <1264030852@qq.com>
2024-09-02 23:49:41 +0800

    changes

--- components/Live.tsx
+++ components/Live.tsx
@@ -1,18 +1,69 @@
 import React, { Key, use, useCallback, useEffect, useState } from "react";
 import LiveCursor from "./cursor/LiveCursor";
-import { useMyPresence, useOthers } from "@liveblocks/react";
+import {
+  useBroadcastEvent,
+  useEventListener,
+  useMyPresence,
+  useOthers
+} from "@liveblocks/react";
 import CursorChat from "./cursor/CursorChat";
-import { CursorMode, Reaction } from "@/types/type";
-import ReactionSelector from "./reaction/FlyingReaction";
+import { CursorMode, CursorState, Reaction, ReactionEvent } from "@/types/type";
+import ReactionSelector from "./reaction/ReactionButton";
+import FlyingReaction from "./reaction/FlyingReaction";
+import useInterval from "@/hooks/useInterval";
 
 const Live = () => {
   const others = useOthers();
 
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
 
-  const [cursorState, setCursorState] = useState({ mode: CursorMode.Hidden });
+  const [cursorState, setCursorState] = useState<CursorState>({
+    mode: CursorMode.Hidden
+  });
 
   const [reaction, setReaction] = useState<Reaction[]>([]);
+
+  // broadcast event to others in the room
+  const broadcast = useBroadcastEvent();
+
+  // reaction 按e 点击一堆表情
+  useInterval(() => {
+    if (
+      cursorState.mode === CursorMode.Reaction &&
+      cursorState.isPressed &&
+      cursor
+    ) {
+      setReaction(reactions =>
+        reactions.concat([
+          {
+            point: { x: cursor.x, y: cursor.y },
+            value: cursorState.reaction,
+            timestamp: Date.now()
+          }
+        ])
+      );
+
+      broadcast({
+        x: cursor.x,
+        y: cursor.y,
+        value: cursorState.reaction
+      });
+    }
+  }, 100);
+
+  // 监听共同页面的操作
+  useEventListener(eventData => {
+    const event = eventData.event as ReactionEvent;
+    setReaction(reactions =>
+      reactions.concat([
+        {
+          point: { x: event.x, y: event.y },
+          value: event.value,
+          timestamp: Date.now()
+        }
+      ])
+    );
+  });
 
   const handlePointserMove = useCallback((event: React.PointerEvent) => {
     event.preventDefault();
@@ -34,13 +85,34 @@ const Live = () => {
     updateMyPresence({ cursor: null, message: null });
   }, []);
 
-  const handlePointserDown = useCallback((event: React.PointerEvent) => {
-    // 去掉preventDefault
-    const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
+  const handlePointserDown = useCallback(
+    (event: React.PointerEvent) => {
+      // 去掉preventDefault
+      const x = event.clientX - event.currentTarget.getBoundingClientRect().x;
 
-    const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
-    updateMyPresence({ cursor: { x, y } });
-  }, []);
+      const y = event.clientY - event.currentTarget.getBoundingClientRect().y;
+      updateMyPresence({ cursor: { x, y } });
+
+      setCursorState((state: CursorState) =>
+        cursorState.mode === CursorMode.ReactionSelector
+          ? { ...state, isPressed: true }
+          : state
+      );
+    },
+    [cursorState.mode, setCursorState]
+  );
+
+  const handlePointerUp = useCallback(
+    (event: React.PointerEvent) => {
+      setCursorState((state: CursorState) =>
+        cursorState.mode === CursorMode.ReactionSelector
+          ? { ...state, isPressed: true }
+          : state
+      );
+    },
+    // recaculate the state
+    [cursorState.mode, setCursorState]
+  );
 
   useEffect(() => {
     const onKeyUp = (e: KeyboardEvent) => {
@@ -54,6 +126,7 @@ const Live = () => {
         updateMyPresence({ message: "" });
         setCursorState({ mode: CursorMode.Hidden });
       } else if (e.key === "e") {
+        console.log("Ee");
         setCursorState({
           mode: CursorMode.ReactionSelector
         });
@@ -61,7 +134,6 @@ const Live = () => {
     };
 
     const onKeyDown = (e: KeyboardEvent) => {
-      console.log(23423);
       if (e.key === "/") {
         e.preventDefault();
       }
@@ -76,14 +148,29 @@ const Live = () => {
     };
   }, [updateMyPresence]);
 
+  const setReactions = useCallback((reaction: string) => {
+    setCursorState({ mode: CursorMode.Reaction, reaction, isPressed: false });
+  }, []);
+
   return (
     <div
       onPointerMove={handlePointserMove}
       onPointerLeave={handlePointserLeave}
       onPointerDown={handlePointserDown}
+      onPointerUp={handlePointerUp}
       className="h-[100vh] w-full flex justify-center items-center text-center"
     >
-      <h1 className="text-5xl text-white">Liveblocks figma clone</h1>
+      <h1 className="text-2xl text-white">Liveblocks figma clone</h1>
+
+      {reaction.map(r => (
+        <FlyingReaction
+          key={r.timestamp.toString()}
+          x={r.point.x}
+          y={r.point.y}
+          timestamp={r.timestamp}
+          value={r.value}
+        />
+      ))}
 
       {cursor && (
         <CursorChat
@@ -95,11 +182,7 @@ const Live = () => {
       )}
 
       {cursorState.mode === CursorMode.ReactionSelector && (
-        <ReactionSelector
-          setReaction={reaction => {
-            setReaction(reaction);
-          }}
-        />
+        <ReactionSelector setReaction={setReactions} />
       )}
 
       <LiveCursor others={others} />
--- components/reaction/FlyingReaction.tsx
+++ components/reaction/FlyingReaction.tsx
@@ -1,42 +1,25 @@
-import React from "react";
+import styles from "./index.module.css";
 
 type Props = {
-  setReaction: (reaction: string) => void;
+  x: number;
+  y: number;
+  timestamp: number;
+  value: string;
 };
 
-export default function ReactionSelector({ setReaction }: Props) {
+export default function FlyingReaction({ x, y, timestamp, value }: Props) {
   return (
     <div
-      className="-translate-x-1/2 -translate-y-1/2 transform rounded-full bg-white px-2"
-      style={{
-        boxShadow:
-          "0 0 0 0.5px rgba(0, 0, 0, 0.08), 0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)"
-      }}
-      onPointerMove={e => e.stopPropagation()}
+      className={`pointer-events-none absolute select-none ${
+        styles.disappear
+      } text-${(timestamp % 5) + 2}xl ${styles["goUp" + (timestamp % 3)]}`}
+      style={{ left: x, top: y }}
     >
-      <ReactionButton reaction="👍" onSelect={setReaction} />
-      <ReactionButton reaction="🔥" onSelect={setReaction} />
-      <ReactionButton reaction="😍" onSelect={setReaction} />
-      <ReactionButton reaction="👀" onSelect={setReaction} />
-      <ReactionButton reaction="😱" onSelect={setReaction} />
-      <ReactionButton reaction="🙁" onSelect={setReaction} />
+      <div className={styles["leftRight" + (timestamp % 3)]}>
+        <div className="-translate-x-1/2 -translate-y-1/2 transform">
+          {value}
+        </div>
+      </div>
     </div>
-  );
-}
-
-function ReactionButton({
-  reaction,
-  onSelect
-}: {
-  reaction: string;
-  onSelect: (reaction: string) => void;
-}) {
-  return (
-    <button
-      className="transform select-none p-2 text-xl transition-transform hover:scale-150 focus:scale-150 focus:outline-none"
-      onPointerDown={() => onSelect(reaction)}
-    >
-      {reaction}
-    </button>
   );
 }
